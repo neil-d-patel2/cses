d70e3715d051f7f0004eeda56e24a5b72ba3ad5d
Neil <ebox381@gmail.com>
2026-06-14 18:33:51 -0400

    finished reps

--- problems/repetitions.py
+++ problems/repetitions.py
@@ -5,3 +5,21 @@ import sys
 input = sys.stdin.readline()
 
 
+s = set()
+out = 0
+curLen = 0
+
+for _ in input:
+    if _ in s:
+        curLen += 1
+    else:
+        curLen = 1
+        s.clear()
+        s.add(_)
+
+    if curLen > out:
+        out = curLen
+
+print(out)
+
+
