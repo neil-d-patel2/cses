4290ead0f1c3a9268837e8175b21759fc3915621
Neil <ebox381@gmail.com>
2026-06-14 00:41:36 -0400

    works

--- weirdalgorith.py
+++ weirdalgorith.py
@@ -1,8 +1,17 @@
 import sys
 
 
-input = sys.stdin.readline()
+n = int(sys.stdin.readline())
 
+print(n, end = " ")
+
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = n*3 + 1
+
+    print(n, end = " ")
 
 
 
